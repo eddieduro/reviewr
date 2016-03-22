73350b16dd93011a21d9a3c4c15be5217f7ffb34
eddie <e.jamesduro@gmail.com>
2016-03-21 22:13:56 -0700

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: ` <div class="container-fluid">
+  				<h1>Skeleton Angular2 App!</h1>
+    		<div>`
+})
+export class AppComponent { }
